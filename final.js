3d37ab9d6408ccfac05d550839a450c1edf07404
Carter Cingrani <cingranicj@gmail.com>
2019-12-16 09:39:06 -0700

    work on js

--- final.js
+++ final.js
@@ -1,3 +1,8 @@
+let li = document.getElementsByTagName('ul') [0];
+let purchaseBox = document.getElementById("Purchases");
+let purchaseButton = document.getElementById("addPurchase");
+
+
 class BudgetingTool {
     constructor() {
         this.user = new ToolUser();
